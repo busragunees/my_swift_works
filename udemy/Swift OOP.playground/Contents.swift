e3a8b02174c6ac9aa3cf1cded00d099ee8dabd61
Busra Gunes <busraaguunes@gmail.com>
2022-07-04 18:12:44 +0300

    I've moved on to the issue of inheritance.

--- udemy/Swift OOP.playground/Contents.swift
+++ udemy/Swift OOP.playground/Contents.swift
@@ -565,6 +565,147 @@ Asinifi.x
 Asinifi.metod()
 
 
-//---------ENUMERATİON
+//---------ENUMERATİON:
+
+//sonrasında bak.
 
 
+
+
+
+
+
+
+//----------COMPOSITION:
+
+class Kisilerr{
+    var ad:String?
+    var yas:Int?
+    /*var il:String?
+    var ilce:String?        burada bunları kullanmak güzel bir kodlama değil. bunun yerine il ve ilçe için baska bir class olusturup burada onu ref almak daha mantıklı.  */
+    var adres:Adres?
+    
+    init(ad:String,yas:Int,adres:Adres) {
+        self.ad = ad
+        self.yas = yas
+        self.adres = adres
+    }
+    
+}
+
+
+class Adres{
+    var il:String?
+    var ilce:String?
+    init(il:String, ilce:String) {
+        self.il = il
+        self.ilce = ilce
+    }
+}
+
+let adres = Adres(il: "İstanbul", ilce: "Zeytinburnu")
+let kisi = Kisilerr(ad: "Büşra", yas: 24, adres: adres)
+
+print("Kişi adres :\(kisi.adres!.il!)")
+
+
+//öncelikle alt sınıfları oluşturmak daha mantıklı olacaktır.
+
+class Kategoriler{
+    var kategori_id:Int?
+    var kategori_ad:String?
+    
+    init(kategori_id:Int, kategori_ad:String) {
+        self.kategori_ad = kategori_ad
+        self.kategori_id = kategori_id
+    }
+}
+
+class Yonetmenler{
+    var yonetmen_id:Int?
+    var yonetmen_ad:String?
+    
+    init(yonetmen_id:Int, yonetmen_ad:String) {
+        self.yonetmen_ad = yonetmen_ad
+        self.yonetmen_id = yonetmen_id
+    }
+}
+
+class Filmler{
+    var film_id:Int?
+    var film_ad:String?
+    var film_yil:Int?
+    var kategori:Kategoriler?
+    var yonetmen:Yonetmenler?
+    
+    init(film_id:Int,film_ad:String,film_yil:Int,kategori:Kategoriler,yonetmen:Yonetmenler) {
+        self.film_id = film_id
+        self.film_ad = film_ad
+        self.film_yil = film_yil
+        self.kategori = kategori
+        self.yonetmen = yonetmen
+}
+}
+
+var k1 = Kategoriler(kategori_id: 1, kategori_ad: "Dram")
+var k2 = Kategoriler(kategori_id: 2, kategori_ad: "Komedi")
+var k3 = Kategoriler(kategori_id: 3, kategori_ad: "Bilim Kurgu")
+
+var y1 = Yonetmenler(yonetmen_id: 1, yonetmen_ad: "Nuri Bilge Ceylan")
+var y2 = Yonetmenler(yonetmen_id: 2, yonetmen_ad: "Quetin Tarantino")
+var y3 = Yonetmenler(yonetmen_id:3, yonetmen_ad: "Christopher Nolan")
+
+var f1 = Filmler(film_id: 1, film_ad: "Django", film_yil: 2013, kategori: k1, yonetmen: y2)
+var f2 = Filmler(film_id: 2, film_ad: "Inception", film_yil: 2006, kategori: k3, yonetmen: y3)
+
+print("Film id: \(f1.film_id!)")
+print("Film ad: \(f1.film_ad!)")
+print("Film yıl: \(f1.film_yil!)")
+print("Film kategori: \(f1.kategori!.kategori_ad!)")
+print("Film yonetmen: \(f1.yonetmen!.yonetmen_ad!)")
+
+
+//genellikle veritabanlarında kullanırız.
+
+
+//----------------------------------------------INHERITANCE(MİRAS-KALITIM)-------------------------------
+/*
+ -Bir class özelliklerini baska bir sınıfa aktarabilir.
+ 
+ Aktaran class = superclass
+ Alt class = subclass
+ 
+ -Bir class sadece bir classtan kalıtım alabilir. Birden fazla classtan kalıtım alamaz.
+ */
+
+class Arac {
+    var renk:String?
+    var vites:String?
+    
+    init(renk:String, vites:String) {
+        self.renk = renk
+        self.vites = vites
+    }
+}
+
+class Araba1:Arac{
+    var kasaTipi:String?
+    
+    init(kasaTipi:String,renk:String,vites:String) {
+        self.kasaTipi = kasaTipi
+        super.init(renk:renk, vites:vites)
+    }
+}
+
+class Honda:Araba1{
+    var model:String?
+    
+    init(model:String,kasaTipi:String,renk:String,vites:String) {
+        self.model = model
+        super.init(kasaTipi: kasaTipi, renk: renk, vites: vites)
+    }
+}
+
+var araba = Araba1(kasaTipi: "Sedan", renk: "Beyaz", vites: "Otomatik")
+
+var arac = Arac(renk: "Kırmızı", vites: "Manuel")
