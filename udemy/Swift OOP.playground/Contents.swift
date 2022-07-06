9db2a52a1da6fe6b0df2d7f68fe0137ca6abf740
Busra Gunes <busraaguunes@gmail.com>
2022-07-06 23:23:39 +0300

    i solved inheritance example

--- udemy/Swift OOP.playground/Contents.swift
+++ udemy/Swift OOP.playground/Contents.swift
@@ -709,3 +709,40 @@ class Honda:Araba1{
 var araba = Araba1(kasaTipi: "Sedan", renk: "Beyaz", vites: "Otomatik")
 
 var arac = Arac(renk: "Kırmızı", vites: "Manuel")
+
+
+
+//INHERITANCE EXAMPLE:
+
+class Ev{
+    var pencereSayisi:Int?
+    
+    init(pencereSayisi:Int) {
+        self.pencereSayisi = pencereSayisi
+    }
+}
+
+class Saray:Ev{
+    var kuleSayisi:Int?
+    
+    init(kuleSayisi:Int, pencereSayisi:Int) {
+        self.kuleSayisi = kuleSayisi
+        super.init(pencereSayisi:pencereSayisi)
+    }
+}
+
+class Villa:Ev{
+    var garajVarmi:Bool?
+    
+    init(garajVarmi:Bool, pencereSayisi:Int) {
+        self.garajVarmi = garajVarmi
+        super.init(pencereSayisi: pencereSayisi)
+    }
+}
+
+var topkapiSarayi = Saray(kuleSayisi: 5, pencereSayisi: 30)
+var bogazVilla = Villa(garajVarmi: true, pencereSayisi: 10)
+
+print(topkapiSarayi.kuleSayisi!)
+print(topkapiSarayi.pencereSayisi!)
+
